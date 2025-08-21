a18374e521d8542e84ea42c0e7da25ace73093a4
alibabakutty <tallysriram007@gmail.com>
2025-08-21 10:00:23 +0530

    first backup for github

---
+++ android/app/src/main/kotlin/com/example/maruti_kirba_lighting_solutions/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.maruti_kirba_lighting_solutions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:maruti_kirba_lighting_solutions/firebase_options.dart';
+import 'package:maruti_kirba_lighting_solutions/pages/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
